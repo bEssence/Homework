0aed5c187f1a9a1e217f70e2674de6575ee3e1e5
bahecka <baheckaessence@gmail.com>
2022-02-17 17:22:22 +0700

    Little changes.

--- HW/HW/Program.cs
+++ HW/HW/Program.cs
@@ -1,16 +1,12 @@
-﻿Console.WriteLine("Как вас зовут?");
-
-Console.WriteLine();
+﻿Console.WriteLine("Как вас зовут?\n");
 
 string? name = Console.ReadLine();
 
-Console.WriteLine();
-
-Console.WriteLine($"Здравствуйте {name}, приветствую вас в игре 'Угадай число'.\n" +
+Console.WriteLine($"\nЗдравствуйте {name}, приветствую вас в игре 'Угадай число'.\n" +
                   $"Ваша цель - угадать число в промежутке от 0 до 99, удачи!");
 
 Random random = new Random();
-int hiddenNumber = random.Next(0,100);
+int hiddenNumber = random.Next(0, 100);
 int userNumber = -1;
 int amountOfAttempts = 0;
 bool userWon = false;
@@ -27,30 +23,27 @@ while (!userWon)
                           "Попробуйте снова.");
         continue;
     }
-    else if(userNumber>100 || userNumber <= 0) 
+    else if (userNumber > 100 || userNumber <= 0)
     {
         Console.WriteLine("Число выходит за гранцы промежутка, вы будете арестованы!");
         continue;
     }
-    amountOfAttempts += 1;
 
-    Console.WriteLine();
+    amountOfAttempts += 1;
 
     if (userNumber == hiddenNumber)
     {
-        Console.WriteLine("Поздравляем, вы угадали число!");
-        Console.WriteLine($"Количество попыток - {amountOfAttempts}");
+        Console.WriteLine("\nПоздравляем, вы угадали число!" +
+                         $"\nКоличество попыток - { amountOfAttempts}");
         userWon = true;
     }
-
     else if (userNumber < hiddenNumber)
     {
-        Console.WriteLine("Ваше число меньше загаданного.");
+        Console.WriteLine("\nВаше число меньше загаданного.");
     }
-
     else
     {
-        Console.WriteLine("Ваше число больше загаданного.");
+        Console.WriteLine("\nВаше число больше загаданного.");
     }
 }
 Console.ReadLine();
